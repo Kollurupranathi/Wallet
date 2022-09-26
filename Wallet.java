71f566bbe93fd38e346d5b298eefbca60690631e
Kollurupranathi <kpranathi009@gmail.com>
2022-09-26 11:40:57 +0530

    added constructor

--- Wallet.java
+++ Wallet.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 class WalletPay{
-    private float amount=0;
+ 
+    private float amount;
+
+    public WalletPay(){
+        this.amount=0;
+    }
+    public WalletPay(float cash){
+        this.amount=cash;
+    }
     
     public float addCash(float cash){
         if(cash>=0){return (amount +=cash);}
@@ -8,15 +16,22 @@ class WalletPay{
 
     public float payCash(float cash){
         if((cash>=0)&& (cash<=amount)){return (amount -=cash);}
-        else if ((cash>=0)&& (cash>amount)){return Float.parseFloat("Insufiicient Balance");}
+        else if (cash>amount){return Float.parseFloat("Insufiicient Balance");}
         return Float.parseFloat("Amount Cannot be negative");}
-        
+
     public float CheckBalance(){return amount; }
 
 }
 public class Wallet {
     public static void main(String[] args){
-        WalletPay obj = new WalletPay();
+        Scanner x= new Scanner(System.in);
+        String str = x.nextLine();
+        int y;
+        if(str.equals("")){y=0;}
+        else{y =Integer.parseInt(str);}
+        
+        WalletPay obj = new WalletPay(y);
+        
         Scanner in = new Scanner(System.in);
         
         while(in.hasNextLine()){
@@ -28,9 +43,10 @@ public class Wallet {
                 else if (arr[0].equals("pay")){System.out.println(obj.payCash(Float.parseFloat(arr[1])));}
                 else if (arr[0].equals("Show Balance")){System.out.println(obj.CheckBalance());}
                 else{
-                    System.out.println("Enter any Query");
+                    System.out.println("Enter a valid Query");
                 }  
             }
         }
+        x.close();
         in.close();   
 }}
